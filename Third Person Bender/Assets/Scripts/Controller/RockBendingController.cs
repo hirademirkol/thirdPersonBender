41e35cf71ded9bfa5e839bf6c93b718f437b3702
Hira Demirkol <hirayigitdemirkol@gmail.com>
2021-11-24 16:54:43 +0100

    Raise rock from ground when instancing, make slight camera adjustment

--- Third Person Bender/Assets/Scripts/Controller/RockBendingController.cs
+++ Third Person Bender/Assets/Scripts/Controller/RockBendingController.cs
@@ -7,6 +7,7 @@ public class RockBendingController : MonoBehaviour
     public GameObject RockObject;
     public float PushForce = 50f;
     public Transform Camera;
+    public float RaiseTime = 0.25f;
 
     private GameObject _rock;
     private bool _rockDrawn = false;
@@ -20,14 +21,17 @@ public class RockBendingController : MonoBehaviour
     IEnumerator DrawRock()
     {
         _rock = GameObject.Instantiate(RockObject, transform);
-        _rock.transform.localPosition =  new Vector3(2.5f, 1f, 0f);
+        _rock.transform.localPosition = new Vector3(2f, 0f, 0f);
         _rockDrawn = true;
+        StartCoroutine(RaiseRock(_rock, RaiseTime));
         yield return null;
     }
 
     IEnumerator PushRock()
     {
         var rigidbody = _rock.GetComponent<Rigidbody>();
+        rigidbody.GetComponent<Collider>().enabled = true;
+        rigidbody.isKinematic = false;
         _rock.transform.SetParent(null);
         Vector3 force = Quaternion.Euler(Camera.eulerAngles.x, 0f, 0f) * new Vector3(0f, PushForce , 0f);
         rigidbody.AddRelativeForce(force);
@@ -35,5 +39,18 @@ public class RockBendingController : MonoBehaviour
         _rock.GetComponent<Rock>().Activate();
         _rockDrawn = false;
         yield return null;
+    }
+
+    IEnumerator RaiseRock(GameObject rock, float time)
+    {
+        var moveVector = new Vector3(0f, 2f/time, 0f);
+        while (time > 0 && _rockDrawn)
+        {
+            rock.transform.localPosition += moveVector * Time.deltaTime;
+            time -= Time.deltaTime;
+            yield return null;
+        }
+        if(_rockDrawn)
+            rock.transform.localPosition = new Vector3(2f, 2f, 0f);
     }
 }
